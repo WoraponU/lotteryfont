d7fec362c676c00272e3d30fdfe21fbccde1cea1
One <pattanapong.m@hobbiz.co.th>
2017-07-05 11:27:02 +0700

    Header TH|EN

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -6,18 +6,34 @@ import styles from './Header.scss'
 
 const Header = ({ isLoggedIn, logout }) => (
   <header className={styles.header}>
-    <div className="container">
-   
-    <img src="assets/images/logo.png" alt=""/>
-   
+    <nav className="navbar">
+  
+      <div className="container">
+        <div className="navbar-header">
+          <a href="" className="logo">
+              <img src="assets/images/logo.png" alt="Hobbiz"/>
+          </a>
+        </div>
+        <div className="collapse navbar-collapse pull-right">
+          <ul className="list-inline nav navbar-nav navbar-left">
+            <li>
+              <a href="">TH</a>
+            </li>
+            <li>
+              <a href="">EN</a>
+            </li>
+          </ul>
+
+        </div>
     
-    {/*<Link to='/articles' className={styles['nav-link']}>Articles</Link>
-    <Link to='/' className={styles['nav-link']}>หน้าแรก</Link>
-   */}
-    </div>
+
+      </div>
+     </nav>
   </header>
 )
-
+ {/*<Link to='/articles' className={styles['nav-link']}>Articles</Link>
+    <Link to='/' className={styles['nav-link']}>หน้าแรก</Link>
+   */}
 export default connect(
   ({ auth }) => ({
     isLoggedIn: !!auth.token
