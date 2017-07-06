39d524ddee82dc0ea9076907c4b66ca277225793
admin <worapon.p@hobbiz.co.th>
2017-07-06 12:05:34 +0700

    remove babel

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router'
-import {
-  Header
-} from 'Containers'
-import { Home } from 'Components'
+
 import { configureStore } from 'Lib'
+import { Header } from 'Containers'
+import { Home } from 'Components'
 import DevTools from './DevTools'
 
 const store = configureStore()
@@ -14,18 +13,16 @@ export default () => (
   <Provider store={store}>
     <div>
       <Header />
-      
-        <Switch>
-          <Route path='/' exact component={Home} />
-          {/*<Route path='/test' component={Test} />
-          <Route path='/login' component={Signin} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/articles/new' component={CreateArticle} />
-          <Route path='/articles/:id/edit' component={EditArticle} />
-          <Route path='/articles/:id' component={ShowArticle} />
-          <Route path='/articles' component={Articles} />*/}
-        </Switch>
-      
+      <Switch>
+        <Route path='/' exact component={Home} />
+        {/*<Route path='/test' component={Test} />
+        <Route path='/login' component={Signin} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/articles/new' component={CreateArticle} />
+        <Route path='/articles/:id/edit' component={EditArticle} />
+        <Route path='/articles/:id' component={ShowArticle} />
+        <Route path='/articles' component={Articles} />*/}
+      </Switch>
       <DevTools />
     </div>
   </Provider>
