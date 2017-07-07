31eae6d1d5fbf7fd9c0ee7b4b85d495f9b31be93
One <pattanapong.m@hobbiz.co.th>
2017-07-07 13:37:06 +0700

    Header

---
+++ src/components/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Menu.scss'
+import { Nav,NavItem,Grid,Col,Row } from 'react-bootstrap';
+
+const Menu = () => {
+  return (
+    <div>
+      <Grid>
+        <Row className="show-grid">
+          <Col sm={6} md={6} className={styles.position}>
+            <Nav bsStyle="pills" stacked >
+              <NavItem eventKey={1} href="/" title="Item"><span>HOME</span></NavItem>
+              <NavItem eventKey={2} href="/" title="Item"><span>OUR WORK</span></NavItem>
+              <NavItem eventKey={3} href="/" title="Item"><span>ABOUT US</span></NavItem>
+              <NavItem eventKey={4} href="/" title="Item"><span>OUR SERVICES</span></NavItem>
+              <NavItem eventKey={5} href="/" title="Item"><span>OUR CLIENTS</span></NavItem>
+              <NavItem eventKey={6} href="/" title="Item"><span>BLOG</span></NavItem>
+              <NavItem eventKey={7} href="/" title="Item"><span>CONTACT US</span></NavItem>
+              
+            </Nav>
+          </Col>
+        </Row>
+        
+      </Grid>
+    </div>  );
+};
+
+export default Menu;
