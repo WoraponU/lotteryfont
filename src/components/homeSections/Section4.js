343084e8a5cc91219b4447bc1ba2de0b9a858779
One <pattanapong.m@hobbiz.co.th>
2017-07-13 15:49:42 +0700

    section v1

--- src/components/homeSections/Section4.js
+++ src/components/homeSections/Section4.js
@@ -8,26 +8,35 @@ const Section4 = () => {
       <PageHeader>Small teams working on big challenges <br/>
         in honest collaboration with our clients.
       </PageHeader>
-      <Col lg={3} md={2}>
+        <Col lg={2} md={2} >
             <Image src="assets/images/Home/section4/card1.png" className="imgOut" />
-      </Col>
+          </Col>  
       <Grid>
-        <Row >
-         <Col lg={4} md={2} >
+        <Row className="rowPosition">         
+         <Col lg={4} md={4} >
             <Image src="assets/images/Home/section4/card2.png" className="imgW355"/>
           </Col>
+          <Col lg={4} md={4} >
+            <Image src="assets/images/Home/section4/card.png" className="imgCard"/>
+          </Col>
+           <Col lg={4} md={4} className="text-right">
+            <p>	Website and minisite <br/>
+                Branding and visual identity<br/>
+                Marketing material<br/>
+                Digital and offline sale kit</p>
+          </Col>
         </Row>
          <Row >
-         <Col lg={8} md={2} mdOffset={1} >
-            <Image src="assets/images/Home/section4/card5.png" />
+         <Col lg={8} md={4}  >
+            <Image src="assets/images/Home/section4/card5.png"      />
           </Col>
-          <Col lg={3} md={2}>
-            <Image src="assets/images/Home/section4/card3.png" />
+          <Col lg={4} md={4}>
+            <Image src="assets/images/Home/section4/card3.png" className={"imgCard3"} />
           </Col>
         </Row>
          <Row >
-         <Col lg={3} md={2} mdOffset={6}>
-            <Image src="assets/images/Home/section4/card4.png" />
+         <Col lg={3} md={3} mdOffset={4} lgOffset={4}>
+            <Image src="assets/images/Home/section4/card4.png" className={"imgCard4"}/>
           </Col>
          
         </Row>
