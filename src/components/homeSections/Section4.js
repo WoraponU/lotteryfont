6dcdcbaf34975d217cbfd54d99e32152e89fa59b
One <pattanapong.m@hobbiz.co.th>
2017-07-13 16:58:27 +0700

    no message

--- src/components/homeSections/Section4.js
+++ src/components/homeSections/Section4.js
@@ -5,21 +5,21 @@ import './Section4.scss'
 const Section4 = () => {
   return (
     <div className="section4">
-      <PageHeader>Small teams working on big challenges <br/>
-        in honest collaboration with our clients.
-      </PageHeader>
+      <Grid>
+        <PageHeader>Small teams working on big challenges<br/>in honest collaboration with our clients.</PageHeader>
+      </Grid>
       <Col lg={2} md={2} >
         <Image src="assets/images/Home/section4/card1.png" className="imgOut" />
       </Col>  
       <Grid>
         <Row className="rowPosition">         
-         <Col lg={4} md={4} >
+          <Col lg={4} md={4} >
             <Image src="assets/images/Home/section4/card2.png" className="imgCard2"/>
           </Col>
           <Col lg={4} md={4} >
             <Image src="assets/images/Home/section4/card.png" className="imgCard"/>
           </Col>
-           <Col lg={4} md={4} className="text-right">
+          <Col lg={4} md={4} className="text-right">
             <p>	Website and minisite <br/>
                 Branding and visual identity<br/>
                 Marketing material<br/>
@@ -31,12 +31,12 @@ const Section4 = () => {
             <Image src="assets/images/Home/section4/card5.png" />
           </Col>
           <Col lg={4} md={4}>
-            <Image src="assets/images/Home/section4/card3.png" className={"imgCard3"} />
+            <Image src="assets/images/Home/section4/card3.png" className="imgCard3" />
           </Col>
         </Row>
         <Row >
           <Col lg={3} md={3} mdOffset={4} lgOffset={4}>
-            <Image src="assets/images/Home/section4/card4.png" className={"imgCard4"}/>
+            <Image src="assets/images/Home/section4/card4.png" className="imgCard4"/>
           </Col>         
         </Row>
       </Grid>
