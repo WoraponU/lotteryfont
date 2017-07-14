d94522aba8394060645bf135027026ad24a8a56f
One <pattanapong.m@hobbiz.co.th>
2017-07-14 13:54:31 +0700

    no message

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { Section1, Section2, Section3, Section4, Section5, Section6 } from 'Comp
 const Home = () => {
   return (
     <div>
-      <Section1 />
+      <Section1  />
       <Section2 />
       <Section3 />
       <Section4 />
