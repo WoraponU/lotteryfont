0f60ce2a407fc3976dac71d3c5401385ccb5cc62
One <pattanapong.m@hobbiz.co.th>
2017-07-21 15:13:04 +0700

    เก็บรายละเอียด ทำฟิลเตอร์

--- src/components/workSections/WorkSection1.js
+++ src/components/workSections/WorkSection1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Tabs, Tab } from 'react-bootstrap';
 
 import { CardWork } from 'Components/common'
 import LiquidCircle from 'Components/common/LiquidCircle';
@@ -18,13 +18,29 @@ const Section1 = () => {
             <p>We saw a big opportunity to create a personalized user experience to help users attain more accuracy, as well as lower the site’s unnecessarily high abandon rate and boost brand loyalty.</p>             
             <LiquidCircle id={20} radius={189} color={pink} position={["3px", "63px", "auto", "auto"]}/>      
           </Col>
-          <Col lg={4} md={4} sm={4}>
+         <Col md={8}>
+         <Row>
+          <Col lg={12} md={12} sm={12}>
+            <Tabs defaultActiveKey={2} id="Tab">
+              <Tab eventKey={1} title="ALL WORK"></Tab>
+              <Tab eventKey={2} title="POPULAR"></Tab>
+              <Tab eventKey={3} title="PUBLIC"></Tab>
+              <Tab eventKey={4} title="DESKTOP"></Tab>
+              <Tab eventKey={5} title="MOBILE"></Tab>
+            </Tabs>
+          </Col>
+         </Row>
+         <Row>
+          <Col lg={6} md={6} sm={6}>
             <CardWork  img='assets/images/Work/mac5.png' title='MAC-5'/>
           </Col>
-          <Col lg={4} md={4} sm={4}>
+          <Col lg={6} md={6} sm={6}>
               <CardWork  img='assets/images/Work/qualy.png' title='QUALY &amp; CO'/> 
           </Col>
+          </Row>
+        </Col>
         </Row>
+
         <Row className="Row2">
           <Col lg={4} md={4} sm={4}>
             <CardWork  img='assets/images/Work/asap.png' title='ASAP PROJECT'/>
