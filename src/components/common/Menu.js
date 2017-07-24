11e52ee511beff1a38e00f7e6ea239c29cd0005c
One <pattanapong.m@hobbiz.co.th>
2017-07-24 18:08:22 +0700

    ขีดเส้นใต้ Active

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Grid, Col, Row } from 'react-bootstrap';
+import { Grid, Col, Row, Nav } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 import './Menu.scss'
@@ -10,13 +10,15 @@ const Menu = ({ lang, match: { params: { lang: paramLang } } }) => {
     <Grid>
       <Row className="show-grid">
         <Col sm={6} md={6} className="navBar">
-          <NavLink to={`/${paramLang}`} activeClassName="active">{ lang.home }</NavLink>
-          <NavLink to={`/${paramLang}/our-work`} activeClassName="active">{ lang.ourWork }</NavLink>
-          <NavLink to={`/${paramLang}/about-us`} activeClassName="active">{ lang.aboutUs }</NavLink>
-          <NavLink to={`/${paramLang}/client`} activeClassName="active">{ lang.client }</NavLink>
-          <NavLink to={`/${paramLang}/blog`} activeClassName="active">{ lang.blog }</NavLink>
-          <NavLink to={`/${paramLang}/contact`} activeClassName="active">{ lang.contact }</NavLink>
-          <NavLink to={`/${paramLang}/career`} activeClassName="active">{ lang.career }</NavLink> 
+          <Nav bsStyle="pills" stacked className="nav" >
+            <li><NavLink to={`/${paramLang}`} activeClassName="active"><span>{ lang.home }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/our-work`} activeClassName="active"><span>{ lang.ourWork }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/about-us`} activeClassName="active"><span>{ lang.aboutUs }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/client`} activeClassName="active"><span>{ lang.client }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/blog`} activeClassName="active"><span>{ lang.blog }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/contact`} activeClassName="active"><span>{ lang.contact }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/career`} activeClassName="active"><span>{ lang.career }</span></NavLink> </li>
+          </Nav>
         </Col>
       </Row>
     </Grid>
