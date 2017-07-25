5c739a45eb7cea7bdccf0d97ef9f08ce736a90ee
admin <worapon.p@hobbiz.co.th>
2017-07-25 10:58:53 +0700

    loaded asynce content en

--- src/hocs/withLang.js
+++ src/hocs/withLang.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 const withLang = (propName) => (WrappedComponent) => {
   const contentTh = require(`../langs/th/${propName}.json`);
-  const contentEn = require(`../langs/en/${propName}.json`);
   
   return class extends Component {
     constructor() {
@@ -10,31 +9,52 @@ const withLang = (propName) => (WrappedComponent) => {
 
       this.content = {
         th: contentTh,
-        en: contentEn,
       } 
     }
 
     state = {
       content: contentTh,
+      isLoadedContentEn: false,
     }
 
-    changeLang(lang) {
-      this.setState({ 
-        content: this.content[lang], 
-      })    
+    manageLang(lang) {
+      const { isLoadedContentEn } = this.state
+
+      if (!isLoadedContentEn && lang === 'en') {
+        const loadContentEn = new Promise((resolve, reject) => {
+          require([`../langs/en/${propName}.json`], (resp, err) => {
+            if (!!err) reject(err);
+            resolve(resp);
+          });
+        });
+
+        loadContentEn.then((contentEn) => {
+          this.content.en = contentEn;
+          
+          this.setState({ 
+            content: this.content[lang], 
+          })    
+        });
+      } else {
+        this.setState({ 
+          content: this.content[lang], 
+        })    
+      }
+
+
     }
 
     componentDidMount() {
       const { match: { params: { lang: nextLang } } } = this.props;
 
-      this.changeLang(nextLang);
+      this.manageLang(nextLang);
     }
 
     componentWillReceiveProps({ match: { params: { lang: nextLang } } }) {
       const { match: { params: { lang: currentLang } } } = this.props;
 
       if (currentLang !== nextLang ) {
-        this.changeLang(nextLang);
+        this.manageLang(nextLang);
       }
       
     }
