c15029fc25f9d8898a4a4504b26e38667cad24c0
admin <worapon.p@hobbiz.co.th>
2017-07-26 11:41:01 +0700

    แก้หน้า work  out

--- src/components/ourWorkSections/OurWorks.js
+++ src/components/ourWorkSections/OurWorks.js
@@ -21,29 +21,43 @@ const OurWorks = ({ lang: { OurWorks: content } }) => {
             </Tabs>
           </Col>
         </Row>
+
+
         <Row>
-        <Col lgOffset={4} mdOffset={4} lg={4} md={4} sm={4}>
-          <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
-        </Col>
-        <Col lg={4} md={4} sm={4}>
-          <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
-        </Col>
-        </Row>
-        <Row>
-          <Col lg={4} md={4} sm={4}>
-            <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
+          <Col lg={3} md={3} sm={3} className="content">
+            <h1 dangerouslySetInnerHTML={{__html: content.header}}></h1>
+            <hr/>
+            <p>{ content.content }</p>
+            <p>{ content.content1 }</p>
+            <LiquidCircle id={20} radius={189} color={pink} position={["3px", "63px", "auto", "auto"]}/>      
           </Col>
-          <Col lg={4} md={4} sm={4}>
-            <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
-          </Col>
-        </Row>
-        <Row>
-          <Col lgOffset={4} mdOffset={4} lg={4} md={4} sm={4}>
-            <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
-          </Col>
-          <Col lg={4} md={4} sm={4}>
-            <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
-          </Col>
+
+          <div className="slider">
+            <Row>
+              <Col lgOffset={4} mdOffset={4} lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
+              </Col>
+              <Col lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
+              </Col>
+              <Col lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
+              </Col>
+            </Row>
+            <Row>
+              <Col lgOffset={4} mdOffset={4} lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/mac5.png' title='MAC-5'/>
+              </Col>
+              <Col lg={4} md={4} sm={4}>
+                <CardWork img='/assets/images/Work/chula.png' title='CHULA'/>            
+              </Col>
+            </Row>
+          </div>
         </Row>
       </Grid>
     </div>
