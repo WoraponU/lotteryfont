502cb8b270d1e3f27c9eb936574c7bd8d6121757
admin <worapon.p@hobbiz.co.th>
2017-08-03 16:35:18 +0700

    no message

--- src/common/containers/homeSections/Section7.js
+++ src/common/containers/homeSections/Section7.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 import { Section7Component } from 'Components/homeSections'
 import { withLang } from 'Hocs';
 
 class Section7 extends Component {
+  componentDidMount() {
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/mail/send-mail',
+    })
+    .then((resp) => {
+      console.log(resp);
+    });
+  }
   render() {
     const { lang } = this.props;
 
