19d78e909c88d84fe7499420f7b3804390077769
admin <worapon.p@hobbiz.co.th>
2017-08-03 17:03:58 +0700

    no message

--- src/common/containers/homeSections/Section7.js
+++ src/common/containers/homeSections/Section7.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { compose } from 'recompose'
 import { withRouter } from 'react-router-dom'
-import axios from 'axios'
+import { createArticle }  from 'Actions'
+
+// import axios from 'axios'
 
 import { Section7Component } from 'Components/homeSections'
 import { withLang } from 'Hocs';
 
 class Section7 extends Component {
-  componentDidMount() {
-    axios({
-      method: 'post',
-      url: 'http://localhost:3000/api/mail/send-mail',
-    })
-    .then((resp) => {
-      console.log(resp);
-    });
-  }
+  // componentDidMount() {
+  //   axios({
+  //     method: 'post',
+  //     url: 'http://localhost:3000/api/mail/send-mail',
+  //   })
+  //   .then((resp) => {
+  //     console.log(resp);
+  //   });
+  // }
   render() {
     const { lang } = this.props;
 
@@ -27,6 +30,14 @@ class Section7 extends Component {
 
 const enhance = compose(
   withRouter,
+  connect(
+    null,
+    dispatch => ({
+      sendEmailContactUs(dataEmailForm) {
+        dispatch(createArticle(value))
+      }
+    }),
+  ),
   withLang('home/Section7')
 );
 
