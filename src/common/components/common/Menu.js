377a18452af15888ccdeaeca525fd4df09860300
One <pattanapong.m@hobbiz.co.th>
2017-08-08 17:14:54 +0700

    เมนูเดิม

--- src/common/components/common/Menu.js
+++ src/common/components/common/Menu.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
-
-import { Grid, Col, Row,Nav,NavItem,Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { Grid, Col, Row } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 import './Menu.scss'
@@ -12,23 +10,15 @@ const Menu = ({ lang, match: { params: { lang: paramLang } } }) => {
     <Grid>
       <Row className="show-grid">
         <Col sm={6} md={6} className="navBar">
-        <Navbar.Collapse>
-          <Nav stacked>
-            <LinkContainer  exact to={`/${paramLang}`} activeClassName="active"><NavItem ><span>{ lang.home }</span></NavItem></LinkContainer>
-            <LinkContainer to={`/${paramLang}/aboutUs`} activeClassName="active"><NavItem ><span>{ lang.aboutUs }</span></NavItem></LinkContainer>  
-
-            <LinkContainer to={`/${paramLang}/our-work`} activeClassName="active"><NavItem ><span>{ lang.ourWork }</span></NavItem></LinkContainer>  
-
-            {/* <LinkContainer to={`/${paramLang}`} activeClassName="active"><NavItem ><span>{ lang.client }</span></NavItem></LinkContainer>  
-
-            <LinkContainer to={`/${paramLang}`} activeClassName="active"><NavItem ><span>{ lang.blog }</span></NavItem></LinkContainer>   */}
-
-            <LinkContainer to={`/${paramLang}/contact`} activeClassName="active"><NavItem ><span>{ lang.contact }</span></NavItem></LinkContainer>  
-
-            {/* <LinkContainer to={`/${paramLang}`} activeClassName="active"><NavItem ><span>{ lang.career }</span></NavItem></LinkContainer>   */}
-
-          </Nav>
-        </Navbar.Collapse>
+          <ul>
+            <li><NavLink exact to={`/${paramLang}`} activeClassName="active"><span>{ lang.home }</span></NavLink> </li>
+            <li><NavLink to={`/${paramLang}/aboutUs`} activeClassName="active"><span>{ lang.aboutUs }</span></NavLink></li>
+            <li><NavLink to={`/${paramLang}/our-work`} activeClassName="active"><span>{ lang.ourWork }</span></NavLink></li>
+            <li><a disabled className="disabled"><span>{ lang.client }</span></a></li>
+            <li><a disabled className="disabled"><span>{ lang.blog }</span></a></li>
+            <li><NavLink to={`/${paramLang}/contact`} activeClassName="active"><span>{ lang.contact }</span></NavLink></li>
+            <li><a disabled className="disabled"><span>{ lang.career }</span></a></li>  
+          </ul>
         </Col>
       </Row>
     </Grid>
--- src/common/components/common/Navbar.js
+++ src/common/components/common/Navbar.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { Navbar, Image, Col } from 'react-bootstrap';
-import { Menu } from 'Containers/common'
+import { Navbar, Image } from 'react-bootstrap';
 
 import './Navbar.scss';
 
-const NavbarComponent = ({ onChangeLang, match: { params: { lang } }}) => {
-  
+const NavbarComponent = ({ onChangeLang, match: { params: { lang } } }) => {
   return (
     <div className="header">
-      <Navbar collapseOnSelect>      
+      <Navbar>      
         <Navbar.Brand>
             <Image src="/assets/images/common/logoHobbiz.png"/>           
-        </Navbar.Brand>   
-         <Navbar.Toggle />        
-         <Col lgHidden mdHidden>
-            <Menu />
-          </Col>
-         <Navbar.Collapse>
-          
-          <div className="navMenu">  
+        </Navbar.Brand>       
+        <div className="navMenu">  
           <a href="#" className={lang === 'th' ? 'active' : ''} onClick={(e) => onChangeLang(e, 'th')}>th</a>
           <span>|</span>
           <a href="#" className={lang === 'en' ? 'active' : ''} onClick={(e) => onChangeLang(e, 'en')}>en</a> 
-        </div> 
-        
-        </Navbar.Collapse>
-     
+        </div>
+       
       </Navbar>
     </div>
   );
