4c4e8665bbb829d2da3585bd7023d5715dd6dac8
One <pattanapong.m@hobbiz.co.th>
2017-08-15 10:53:51 +0700

    รีวิวโค๊ดแล้ว

--- src/common/components/clientsSections/Clients.js
+++ src/common/components/clientsSections/Clients.js
@@ -10,10 +10,10 @@ import './Clients.scss'
 const Clients = ({lang: { Clients: content } }) => {
   return (
     <Grid className="clients">
-      <Row>         
-        <Col sm={12} md={12} lg={12} className="text-center">
-          <PopCircle color={blue} radius={118} position={["170px", "-110px", "auto", "auto"]}/>
-          <PopCircle color={yellow} radius={86} position={["280px", "3px", "auto", "auto"]}/>
+      <Row>   
+        <PopCircle color={blue} radius={118} position={["170px", "-110px", "auto", "auto"]}/>
+        <PopCircle color={yellow} radius={86} position={["280px", "3px", "auto", "auto"]}/>      
+        <Col sm={12} md={12} lg={12} className="text-center">  
           <h1 className="header">{content.header}</h1>      
           <Image src="/assets/images/common/line.png"></Image>    
         </Col>
@@ -26,33 +26,33 @@ const Clients = ({lang: { Clients: content } }) => {
       </Row>
       <div className="groupCircle">     
         <Row>
-          <Col lg={2}>
+          <Col sm={2} md={2} lg={2}>
             <CircleClient img='spring'/> 
           </Col>
-          <Col lg={2}  lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='ygg'/> 
           </Col>
-          <Col lg={2} lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='plastic-club'/> 
           </Col>
         </Row>
         <Row>
           <PopCircle color={pink} radius={96} position={["400px", "auto", "auto", "-155px"]}/>  
-          <Col lg={2} lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2}>
             <CircleClient img='qualy-co'/> 
           </Col>
-          <Col lg={2}  lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='bps'/> 
           </Col>
-          <Col lg={2} lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='bsu-thailand'/> 
           </Col>
         </Row>
         <Row>
-          <Col lg={2}>
+          <Col sm={2} md={2} lg={2}>
             <CircleClient img='mac-5'/> 
           </Col>
-          <Col lg={2}  lgOffset={2}>
+          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='double'/> 
           </Col>
         </Row>
