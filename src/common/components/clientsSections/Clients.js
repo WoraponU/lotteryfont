51f9fb5fae62e56e3157beae713da318d865d267
One <pattanapong.m@hobbiz.co.th>
2017-08-16 16:29:42 +0700

    หน้า clients Resp

--- src/common/components/clientsSections/Clients.js
+++ src/common/components/clientsSections/Clients.js
@@ -11,8 +11,8 @@ const Clients = ({lang: { Clients: content } }) => {
   return (
     <Grid className="clients">
       <Row>   
-        <PopCircle color={blue} radius={118} position={["170px", "-110px", "auto", "auto"]}/>
-        <PopCircle color={yellow} radius={86} position={["280px", "3px", "auto", "auto"]}/>      
+        <PopCircle color={blue} radius={118} position={["170px", "0px", "auto", "auto"]}/>
+        <PopCircle color={yellow} radius={86} position={["280px", "150px", "auto", "auto"]}/>      
         <Col sm={12} md={12} lg={12} className="text-center">  
           <h1 className="header">{content.header}</h1>      
           <Image src="/assets/images/common/line.png"></Image>    
@@ -24,35 +24,35 @@ const Clients = ({lang: { Clients: content } }) => {
           <h3 dangerouslySetInnerHTML={{__html: content.title}}></h3> 
         </Col>
       </Row>
-      <div className="groupCircle">     
+      <div className="groupCircle text-center">     
         <Row>
-          <Col sm={2} md={2} lg={2}>
+          <Col xs={12} sm={2} md={2} lg={2}>
             <CircleClient img='spring'/> 
           </Col>
-          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='ygg'/> 
           </Col>
-          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='plastic-club'/> 
           </Col>
         </Row>
         <Row>
           <PopCircle color={pink} radius={96} position={["400px", "auto", "auto", "-155px"]}/>  
-          <Col sm={2} md={2} lg={2} lgOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2}>
             <CircleClient img='qualy-co'/> 
           </Col>
-          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='bps'/> 
           </Col>
-          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='bsu-thailand'/> 
           </Col>
         </Row>
         <Row>
-          <Col sm={2} md={2} lg={2}>
+          <Col xs={12} sm={2} md={2} lg={2}>
             <CircleClient img='mac-5'/> 
           </Col>
-          <Col sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
+          <Col xs={12} sm={2} md={2} lg={2} lgOffset={2} mdOffset={2} smOffset={2}>
             <CircleClient img='double'/> 
           </Col>
         </Row>
