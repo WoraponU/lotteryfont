301d2eb0dfee3da75b9feffd9debe7dc2be7490b
admin <worapon.p@hobbiz.co.th>
2017-08-17 20:05:16 +0700

    ทำแบบพื้นฐานได้

--- src/common/containers/homeSections/Section1.js
+++ src/common/containers/homeSections/Section1.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 import { checkUserAgent }  from 'Actions'
 import { Section1Component } from 'Components/homeSections'
 import { withLang, withUserAgent } from 'Hocs';
 
 class Section1 extends Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/mail/test')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render() {
     const { lang, userAgent } = this.props;
     
