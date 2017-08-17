715c189afd8f8013b6a1f77c738e703a04ad25ec
One <pattanapong.m@hobbiz.co.th>
2017-08-17 13:57:12 +0700

    แก้งานเดี่ยว

--- src/common/components/ourWorkSections/WorkDetail.js
+++ src/common/components/ourWorkSections/WorkDetail.js
@@ -26,6 +26,41 @@ const WorkDetail = (props) => {
     slidesToShow: 2,
     slidesToScroll: 1,
     lazyLoad: false,
+    responsive: [
+       {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {   
+          slidesToShow: 2, 
+          arrows: true,
+          dots: false,
+          infinite: false,
+          variableWidth: false,
+          adaptiveHeight: false,
+          wipeToSlide: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          variableWidth: false,
+          adaptiveHeight: false,
+        }
+      }
+    ]
   };
   const {
     FacebookShareButton,
@@ -44,16 +79,16 @@ const WorkDetail = (props) => {
             <Breadcrumb.Item active>{contentLoaded.name}</Breadcrumb.Item>
           </Breadcrumb>
           <PopCircle color={blue} radius={104} position={["auto", "auto", "-108px", "-147px"]}/>     
-          <Col lgOffset={3} mdOffset={3} smOffset={3} lg={8} md={8} sm={8}>
+          <Col lgOffset={3} mdOffset={2} smOffset={1} lg={8} md={9} sm={10} xs={12}>
             <h1 dangerouslySetInnerHTML={{__html: contentLoaded.mainHeader}}></h1>
           </Col>        
-          <Col lgOffset={3} mdOffset={3} smOffset={3} lg={6} md={6} sm={6}>
+          <Col lgOffset={3} mdOffset={2} smOffset={2} lg={6} md={8} sm={8} xs={12}>
             <h3 dangerouslySetInnerHTML={{__html: contentLoaded.subHeader1}}></h3>
             <p dangerouslySetInnerHTML={{__html: contentLoaded.content1}}></p>
           </Col>
         </Row>
         <Row className="detailImage">
-          <Col lg={2} md={2} sm={2} className="text-right">
+          <Col lg={2} md={2} sm={2} xsHidden className="text-right">
             <Row>
               <h4>{ content.detailImage.clients }</h4>
                 <p dangerouslySetInnerHTML={{__html: contentLoaded.imageInfo.webScreenShot.client}}></p>  
@@ -66,15 +101,17 @@ const WorkDetail = (props) => {
                <p dangerouslySetInnerHTML={{__html: contentLoaded.imageInfo.webScreenShot.year}}></p> 
             </Row>
           </Col>
-          <Col lg={10} md={10} sm={10}>
-                <Image src={`/assets/images/work/workDetail/${contentLoaded.imageInfo.webScreenShot.path}`}/>    
+          <Col lg={10} md={10} sm={10} xs={12}>
+            <Image src={`/assets/images/work/workDetail/${contentLoaded.imageInfo.webScreenShot.path}`}/>    
           </Col>
         </Row>
         <Row className="detailTitle">
-          <PopCircle color={blue} radius={137} position={["auto", "-130px", "50px", "auto"]}/>    
-          <PopCircle color={pink} radius={61} position={["auto", "-150px", "0px", "auto"]}/>     
-          <Col lgOffset={3} mdOffset={3} smOffset={3} lg={8} md={8} sm={8}>
-               <Image src={`/assets/images/work/workDetail/${contentLoaded.imageInfo.logo.path}`}/>   
+          <Col xsHidden>
+            <PopCircle color={blue} radius={137} position={["auto", "0px", "50px", "auto"]}/>    
+            <PopCircle color={pink} radius={61} position={["auto", "0px", "0px", "auto"]}/>   
+          </Col>  
+          <Col lgOffset={3} mdOffset={3} smOffset={2} lg={8} md={8} sm={8} xs={12}>
+              <Image src={`/assets/images/work/workDetail/${contentLoaded.imageInfo.logo.path}`}/>   
             <hr/>
             <h3 dangerouslySetInnerHTML={{__html: contentLoaded.subHeader2}}></h3>
             <p dangerouslySetInnerHTML={{__html: contentLoaded.content2}}></p>
@@ -93,25 +130,25 @@ const WorkDetail = (props) => {
         <Row className="detailTitle">        
           <PopCircle color={blue} radius={121} position={["170px", "0px", "0px", "-193px"]}/>   
           <PopCircle color={purple} radius={84} position={["56px", "0px", "0px", "0px"]}/>   
-          <Col lgOffset={3} mdOffset={3} smOffset={3} lg={6} md={6} sm={6}>
+          <Col lgOffset={3} mdOffset={3} smOffset={3} lg={6} md={6} sm={6} xs={12}>
             <h3 dangerouslySetInnerHTML={{__html: contentLoaded.subHeader3}}></h3>
             <p dangerouslySetInnerHTML={{__html: contentLoaded.content3}}></p>
           </Col>
-          <Col lg={12} md={12} sm={12} className="text-center">
+          <Col lg={12} md={12} sm={12} xs={12} className="text-center">
             <Button className="btn-black mt10">Visit Website</Button>
           </Col>
         </Row>
         <Row className="tabBottom">         
-          <Col lg={1} md={1} sm={1}>
+          <Col lg={1} md={1} sm={1} xs={1}>
             <a href="#" onClick={(e) => onClickOtherWork(e)}>
               <Image src="/assets/images/work/workDetail/arrLeft.png" className="arrow"/>
             </a>            
           </Col>
-          <Col lg={10} md={10} sm={10} className="text-right ">
+          <Col lg={10} md={10} sm={10} xs={9} className="text-right ">
             <FacebookShareButton url={href} children={<Image src="/assets/images/work/workDetail/facebook.png"/>} /> 
             <TwitterShareButton url={href} children={<Image src="/assets/images/work/workDetail/twitter.png"/>} /> 
           </Col>
-          <Col lg={1} md={1} sm={1}>
+          <Col lg={1} md={1} sm={1} xs={1}>
             <a href="#" onClick={(e) => onClickOtherWork(e)}>
               <Image src="/assets/images/work/workDetail/arrRight.png" className="arrow"/>
             </a>
